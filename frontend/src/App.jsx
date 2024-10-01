f930a34dbae369af6fb26aabcf48b5db69ace82c
sanjeetgit1 <sksanjeet616@gmail.com>
2024-10-02 03:02:40 +0530

    Homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,13 @@ import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className=' m-4 sm:mx-[10%]'>
-      
-      <Routes>
+<Navbar/>
+<Routes>
 <Route  path='/'  element={<Home/>} />
 <Route  path='/doctors'  element={<Doctors/>} />
 <Route  path='/doctors/:speciality'  element={<Doctors/>} />
@@ -23,7 +24,7 @@ const App = () => {
 <Route  path='/my-profile'  element={<MyProfile/>} />
 <Route  path='/my-appointments'  element={<MyAppointments/>} />
 <Route  path='/appointment/:docId'  element={<Appointment/>} />
-      </Routes>
+</Routes>
     </div>
   )
 }
