1188c21476ced9027dad83086b76a44fced6829c
sanjeetgit1 <sksanjeet616@gmail.com>
2024-10-07 16:41:31 +0530

    Profile and appointmentpage

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
     const navigate = useNavigate();
   return (
-    <div className='md:mx-10'>
+    <div className='md:mx-10  '>
       <div className=' flex  flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm p-4'>
 
         {/* ------------left side----------- */}
