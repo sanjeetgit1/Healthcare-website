50f0f74f37b6723c4b0ab046d41b3e22a967e461
sanjeetgit1 <sksanjeet616@gmail.com>
2024-10-10 03:42:48 +0530

    databaseconnected

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+ import express from 'express'
+ import cors from 'cors'
+ import 'dotenv/config'
+import connectDB  from './config/mongodb.js'
+
+ // app config
+ const app =express()
+ const port = process.env.PORT || 4000
+
+ 
+
+ // middlewares
+ app.use(express.json())
+ app.use(cors());
+ connectDB();
+ // api endpoints
+ app.get('/',(req,res)=>{
+    res.send('API working Healthcare')
+ })
+
+ app.listen(port,()=>console.log('server started',port));
