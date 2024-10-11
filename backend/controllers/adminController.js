c1c807de3be92d9dfedaad7fb0671673ce7ac2d6
sanjeetgit1 <sksanjeet616@gmail.com>
2024-10-12 00:41:20 +0530

    models

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,12 @@
+
+
+// api  for adding doctor
+
+const addDoctor = async(req,res)=>{
+    try {
+        const { name, email,password, speciality, degree, exprerience,about, fees,address}=req.body
+        
+    } catch (error) {
+        
+    }
+}
